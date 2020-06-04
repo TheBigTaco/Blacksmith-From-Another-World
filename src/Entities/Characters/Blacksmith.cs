8f18d31001c6518f5052c6d618b0c2c4906173b3
Nerdulous5000 <BMWeeks3@gmail.com>
2020-06-03 21:31:32 -0400

    Fixed capitalization error

--- src/Entities/Characters/Blacksmith.cs
+++ src/Entities/Characters/Blacksmith.cs
@@ -3,10 +3,10 @@ using Godot;
 
 public class Blacksmith : Entity {
 
-    public int level { get; set; } = 0;
+    public int Level { get; set; } = 0;
 
     public override void _Ready() {
-        level = 1;
+        Level = 1;
     }
 
     public override void _Process(float delta) {
