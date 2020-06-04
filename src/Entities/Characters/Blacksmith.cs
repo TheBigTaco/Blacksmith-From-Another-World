8c22866b590a45ce818bc468d2cf66bda24f3d2a
Nerdulous5000 <BMWeeks3@gmail.com>
2020-06-03 21:30:34 -0400

    Attached Blacksmith script to characters

--- src/Entities/Characters/Blacksmith.cs
+++ src/Entities/Characters/Blacksmith.cs
@@ -3,10 +3,10 @@ using Godot;
 
 public class Blacksmith : Entity {
 
-    public int Level { get; set; } = 0;
+    public int level { get; set; } = 0;
 
     public override void _Ready() {
-
+        level = 1;
     }
 
     public override void _Process(float delta) {
